fdb956f31f631f5710d38cb76d4f82e34e6d7706
Lyra <nmlyra13@gmail.com>
2025-04-20 20:56:24 -0300

    atua 20-04-25

---
+++ secao16/model/entities/CarRental.java
@@ -0,0 +1,23 @@
+package secao16.model.entities;
+
+import java.time.LocalDateTime;
+
+public class CarRental {
+
+	private LocalDateTime start;
+	private LocalDateTime finish;
+	
+	public CarRental() {
+	}
+	
+	public CarRental(LocalDateTime start, LocalDateTime finish) {
+		this.start = start;
+		this.finish = finish;
+	}
+
+	
+	
+	
+	
+	
+}
