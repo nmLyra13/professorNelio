77afeb5a928fe2fe813355507527b2486391119c
Lyra <nmlyra13@gmail.com>
2025-04-21 14:47:52 -0300

    Atualiz.seção16

--- secao16/model/entities/Invoice.java
+++ secao16/model/entities/Invoice.java
@@ -1,13 +1,13 @@
 package secao16.model.entities;
 
 public class Invoice {
-	
+
 	private Double basicPayment;
 	private Double tax;
-	
+
 	public Invoice() {
 	}
-	
+
 	public Invoice(Double basicPayment, Double tax) {
 		this.basicPayment = basicPayment;
 		this.tax = tax;
@@ -28,7 +28,8 @@ public class Invoice {
 	public void setTax(Double tax) {
 		this.tax = tax;
 	}
+	public Double getTotalPayment() {
+		return getBasicPayment() - getTax();
+	}
 
-	
-	
 }
