05e75af724a2168e881549c0e79989889ae60b3a
Lyra <nmlyra13@gmail.com>
2025-04-21 20:55:58 -0300

    Versão sem interface

--- secao16/model/application/Program.java
+++ secao16/model/application/Program.java
@@ -23,15 +23,15 @@ public class Program {
 		System.out.print("Modelo do Carro: ");
 		String carModel = sc.nextLine();
 
-		System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+		System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
 		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
 
-		System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+		System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
 		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 
 		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 
-		System.out.print("Entre com o preço por hora:");
+		System.out.print("Entre com o preço por hora: ");
 		double pricePerHour = sc.nextDouble();
 		System.out.print("Entre com o preço por dia: ");
 		double pricePerDay = sc.nextDouble();
