848e9ba1566e13950886822b9e4fc379b939081c
Lyra <nmlyra13@gmail.com>
2025-05-06 15:22:36 -0300

    atualização

--- secao17b/services/CalculationService.java
+++ secao17b/services/CalculationService.java
@@ -3,3 +3,13 @@ package secao17b.services;
 public class CalculationService {
 
 }
+
+/*
+ * As we can see, the operation is static. So there is no need to instantiate
+ * the "CalculationService".
+ * 
+ * Como podemos observar. A operação é estática. Então não é preciso instaciar o
+ * "CalculationService".
+ *
+ * 
+ */
