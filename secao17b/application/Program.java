aeaed707cfe7a029cb873b84391867becad520e5
Lyra <nmlyra13@gmail.com>
2025-05-06 12:17:06 -0300

    atualização

--- secao17b/application/Program.java
+++ secao17b/application/Program.java
@@ -1,10 +1,29 @@
 package secao17b.application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		List<Integer> list = new ArrayList<>();
+		
+		String path = "C:\\temp\\in.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		
+			String line = br.readLine();
+			while (line != null) {
+				list.add(Integer.parseInt(line));
+				line = br.readLine();
+				
+				
+			}
+		} catch {
+			
+		}
 	}
-
 }
