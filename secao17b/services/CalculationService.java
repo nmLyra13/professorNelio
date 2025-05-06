3d83c55e0a3c330582c4c38362072a2dade99fcf
Lyra <nmlyra13@gmail.com>
2025-05-06 16:23:22 -0300

    Atualiza��o 2025-05-06 16:23

--- secao17b/services/CalculationService.java
+++ secao17b/services/CalculationService.java
@@ -5,16 +5,22 @@ import java.util.List;
 public class CalculationService {
 
 	/*
-	 *  * As we can see, the operation is static. So there is no need to instantiate
- * the "CalculationService".
+	 * As we can see, the operation is static. So there is no need to instantiate
+	 * the "CalculationService".
 	 */
+	
+	// operation receives a list of integers
 	public static Integer max(List<Integer> list) {
+		// This is a defensive schedule. It cannot be an empty list.
 		if (list.isEmpty()) {
 			throw new IllegalStateException("List can´t be empty");
 		}
+		// get first element from list -> list.get(0)
 		Integer max = list.get(0);
-		for (Integer item: list) {
-			if (item.compareTo(max)>0) {
+		// scan the list
+		for (Integer item : list) {
+			
+			if (item.compareTo(max) > 0) {
 				max = item;
 			}
 		}
