1195ac12afd12be265a3957c19b4a769bc52436b
Lyra <nmlyra13@gmail.com>
2025-05-08 14:58:28 -0300

    atualização

--- secao17d/application/Program.java
+++ secao17d/application/Program.java
@@ -9,26 +9,28 @@ public class Program {
 
 		// Attention: The supertype of any list type is the wildcard "?"
 		// Atenção: O supertipo de qualquer tipo de lista é coringa "?"
-		List<Integer> myInts = Arrays.asList(5,2,10);
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
 		printList(myInts);
 		System.out.println("---------");
 		// let's test a list of strings here
-		List<String> myStrs = Arrays.asList("Maria", "Bob", "Marcos"); 
+		List<String> myStrs = Arrays.asList("Maria", "Bob", "Marcos");
 		printList(myStrs);
 	}
-	
+
 	// This same method here works for any other type of list.
 	// Este mesmo método aqui funciona para qualquer outro tipo de lista.
 	public static void printList(List<?> list) {
 		/*
-		 * We cannot add data to the list because we do not know what type the list is. It gives a compilation error.
+		 * We cannot add data to the list because we do not know what type the list is.
+		 * It gives a compilation error.
 		 * 
-		 * Não podemos adicionar dados a lista, pois não sabemos de que tipo é a lista. Dar erro de compilação.
+		 * Não podemos adicionar dados a lista, pois não sabemos de que tipo é a lista.
+		 * Dar erro de compilação.
 		 * 
 		 * Por exemplo: list.add(3) <= aqui dá erro de compilação.
 		 * 
 		 */
-		for (Object obj: list) {
+		for (Object obj : list) {
 			System.out.println(obj);
 		}
 	}
