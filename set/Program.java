a8588ba9d2e66b2b54feb822de334c66d828ae17
Lyra <nmlyra13@gmail.com>
2025-05-08 21:32:42 -0300

    atualização

---
+++ set/Program.java
@@ -0,0 +1,22 @@
+package set;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Set<T> Set<T> Represents a set of elements (similar to algebra) 
+		 * - Does not allow repetitions; 
+		 * - Elements do not have a position; 
+		 * - Access, insertion and removal of elements are fast; 
+		 * - Provides efficient set operations: intersection, union, difference; 
+		 * - Main implementations: 
+		 * 		a) HashSet: Faster (O(1) operations in hash table) and unordered; 
+		 * 		b) TreeSet: Slower (O(log(n)) operations in red-black tree) and ordered 
+		 * 			by the object's compareTo (or Comparator) 
+		 * c) LinkedHashSet: Intermediate speed and elements in the order they are added.
+		 * 
+		 */
+	}
+
+}
