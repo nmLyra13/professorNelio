383b5358423c926430034cf6dd3ecb8c0946a1bd
Lyra <nmlyra13@gmail.com>
2025-05-12 12:43:39 -0300

    Atualiza��o 2025-05-12 12:43

--- map/Program.java
+++ map/Program.java
@@ -17,7 +17,7 @@ public class Program {
 		cookies.put("phone", "99711122");
 
 		cookies.remove("email");
-		cookies.put("phone","1133");
+		cookies.put("phone", "1133");
 
 		System.out.println("ALL COOKIES: ");
 		for (String key : cookies.keySet()) {
@@ -26,3 +26,29 @@ public class Program {
 	}
 
 }
+
+/*
+ * But what is a map? It is a collection of key/value pairs.
+ * 
+ * Elements are indexed by the key object; 
+ * Elements are indexed by the key object (they have no position) 
+ * Access, insertion and removal of elements are fast.
+ * 
+ * Main implementations: 
+ * HashMap: faster and unordered; 
+ * TreeMap: slower and ordered by the object's compareTo; 
+ * LinkedHashMap: Intermediate speed and elements in the order they are added.
+ * 
+ * Some important methods:
+ *
+ * put(key, value), remove(key), containsKey(key), get(key)
+ * note:
+ * 
+ * a) These are based on equals and hashCode;
+ * b) If equals and hashCode do not exist, pointer comparison is used.
+ * c) clear();
+ * d) size();
+ * e) KeySet() - returns a Set<K>;
+ * f) values() - returns a Collection<V>.	
+ *
+*/
---
+++ map/Program2.java
@@ -0,0 +1,21 @@
+package map;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * String type already has "equals hashCode" and "Compare to" implemented. 
+		 * We can then use "TreeMap<>" directly.
+		 * 
+		 */
+
+		Map<String, String> cookies = new TreeMap<>();
+				
+				
+	}
+
+}
