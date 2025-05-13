5a12032f0054d6550229def4d59263d7cd2dc033
Lyra <nmlyra13@gmail.com>
2025-05-13 11:37:31 -0300

    Atualiza��o 2025-05-13 11:37

---
+++ comparator/MyComparator.java
@@ -0,0 +1,11 @@
+package comparator;
+
+import secao17c.entities.Product;
+
+public class MyComparator implements Comparator<Product>{
+	@Override
+	public int compare(Product p1, Product p2) {
+		return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+	}
+
+}
