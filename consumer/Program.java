e61359d4dcd9d78010cdd1409846a4a6d8748cee
Lyra <nmlyra13@gmail.com>
2025-05-16 20:42:04 -0300

    atualização

--- consumer/Program.java
+++ consumer/Program.java
@@ -17,7 +17,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach();
+		list.forEach(new PriceUpdate());
+		
+		list.forEach(System.out::println);
 	}
 
 }
