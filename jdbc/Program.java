31be2425a9d87eeb0b53bac233cd4b3d7e06fb05
Lyra <130161359+nmLyra13@users.noreply.github.com>
2025-05-24 22:17:52 -0300

    Add files via upload

---
+++ jdbc/Program.java
@@ -0,0 +1,13 @@
+package db;
+
+import java.sql.Connection;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
+	}
+
+}
